c65b224b4bbb7bfe4b07185d2bff148d5e8f30f9
AVGfx <avgfx15@gmail.com>
2025-02-07 23:23:41 +0530

    Add Flowbite-react in client side styling and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,18 @@ import { Route, Routes } from 'react-router-dom';
 import HomeComponent from './Components/HomeComponent/HomeComponent';
 import AboutComponent from './Components/AboutComponent/AboutComponent';
 import SignInSignUPComponent from './Components/SignInSignUPComponent/SignInSignUPComponent';
+import NavbarComponent from './Components/NavbarComponent/NavbarComponent';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<HomeComponent />} />
-      <Route path='/about' element={<AboutComponent />} />
-      <Route path='/signin' element={<SignInSignUPComponent />} />
-    </Routes>
+    <div>
+      <NavbarComponent />
+      <Routes>
+        <Route path='/' element={<HomeComponent />} />
+        <Route path='/about' element={<AboutComponent />} />
+        <Route path='/signin' element={<SignInSignUPComponent />} />
+      </Routes>
+    </div>
   );
 };
 
--- frontend/src/Components/HomeComponent/HomeComponent.jsx
+++ frontend/src/Components/HomeComponent/HomeComponent.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Button } from 'flowbite-react';
+
 const HomeComponent = () => {
-  return <div>HomeComponent</div>;
+  return (
+    <div>
+      HomeComponent
+      <Button gradientDuoTone='pinkToOrange'>Click me</Button>
+    </div>
+  );
 };
 
 export default HomeComponent;
