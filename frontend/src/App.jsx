9f85b12e4b821fa792fe5a9898164b113b126692
AVGfx <avgfx15@gmail.com>
2025-02-07 22:41:19 +0530

    Creating Components like Home, SingIn, About etc and adding react-router-dom for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 // ~ Styling
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomeComponent from './Components/HomeComponent/HomeComponent';
+import AboutComponent from './Components/AboutComponent/AboutComponent';
+import SignInSignUPComponent from './Components/SignInSignUPComponent/SignInSignUPComponent';
 
 const App = () => {
-  return <div className='text-3xl text-red-600'>App</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<HomeComponent />} />
+      <Route path='/about' element={<AboutComponent />} />
+      <Route path='/signin' element={<SignInSignUPComponent />} />
+    </Routes>
+  );
 };
 
 export default App;
